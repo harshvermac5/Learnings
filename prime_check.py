d95a7342b122f17a4d0c77afef5d38ec50a8fea5
harshvermac5 <harshvermac5@gmail.com>
2024-01-11 22:05:20 +0530

    adding some code

--- prime_check.py
+++ prime_check.py
@@ -1,12 +1,9 @@
 #input the number to be checked
 n = int(input("Enter a number: "))
 
-m = (n % i != 0 for i in range(2, int(n**0.5)+1))
-
-print(m)
-"""
-#check if the number is prime
+#If the remainder is not equal to zero for all the numbers in the range,
+#then the input number is a prime number and the value of m will be True
 if n > 1 and all(n % i != 0 for i in range(2, int(n**0.5)+1)):
     print(f"{n} is a prime number")
 else:
-    print(f"{n} is not a prime number")"""
+    print(f"{n} is not a prime number")
