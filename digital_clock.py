273e5cdee0331e9babe1c71a58918d016fbb72c4
harshvermac5 <harshvermac5@gmail.com>
2024-02-23 22:31:33 +0530

    Adding Basic Digital Clock

---
+++ digital_clock.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from time import strftime
+
+window = tk.Tk()
+window.title("Digital Clock")
+window.geometry("640x320")
+
+def time():
+    string = strftime("%H:%M:%S\n%D")
+    label.config(text=string)
+    label.after(1000,time)
+
+label = tk.Label(window, font=("Fira code", 50), background="yellow", foreground= "black")
+label.place(relx=0.5, rely=0.5, anchor="center")
+
+time()
+
+window.mainloop()
