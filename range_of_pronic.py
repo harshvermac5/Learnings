3f7adbd499c80f4e551f3b766c0df5d40d6acd72
harshvermac5 <harshvermac5@gmail.com>
2024-02-26 15:30:02 +0530

    Adding comments

--- range_of_pronic.py
+++ range_of_pronic.py
@@ -1,13 +1,18 @@
+# defining a function that checks for the pronic number in range
+# pronic numbers are those who is product of two conscutive integer, in form of n * (n+1)
 def is_pronic(num):
     for n in range(1, int(num**0.5)+1):
         if n * (n+1) == num:
             return True
     return False
 
+# asking user for range to find pronic of
 x, y = int(input("Enter the number from you want to find Pronic: ")), int(input("Enter the number upto you want to find Pronic: "))
 
+# initialising an empty list
 pronic = []
 
+# checking the pronic, in the defined range
 for i in range(x, y+1):
     if is_pronic(i):
         pronic.append(i)
